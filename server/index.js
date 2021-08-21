d25b2342cb44f98f83f40a7b5bfd5cfcc352f2b7
AaronChu <571388081@qq.com>
2021-08-21 18:30:49 +0800

    feat: 启动express，安装mongodb-driver

--- server/index.js
+++ server/index.js
@@ -1,5 +1,16 @@
 /*
  * @Date: 2021-08-20 20:15:53
  * @LastEditors: AaronChu
- * @LastEditTime: 2021-08-20 20:18:00
+ * @LastEditTime: 2021-08-21 18:21:24
  */
+const express = require('express')
+
+const app = express()
+
+app.get('/', (req, res)=>{
+  res.send('ok')
+})
+
+app.listen(3000, ()=>{
+  console.log(`Example app listening at http://localhost:${3000}`)
+})
