60b01a168abb4ac7c59b2520470a51848882eb76
AaronChuzb <571388081@qq.com>
2021-09-07 01:25:11 +0800

    fix: 修改打包路径

--- admin/vue.config.js
+++ admin/vue.config.js
@@ -1,30 +1,18 @@
 'use strict'
 const path = require('path')
 const defaultSettings = require('./src/settings.js')
+/* const TerserPlugin = require('terser-webpack-plugin') */
 
 function resolve(dir) {
   return path.join(__dirname, dir)
 }
 
-const name = defaultSettings.title || 'vue Admin Template' // page title
-
-// If your port is set to 80,
-// use administrator privileges to execute the command line.
-// For example, Mac: sudo npm run
-// You can change the port by the following methods:
-// port = 9528 npm run dev OR npm run dev --port = 9528
+const name = defaultSettings.title || '战甲秘典小程序后台' // page title
 const port = process.env.port || process.env.npm_config_port || 9528 // dev port
 
 // All configuration item explanations can be find in https://cli.vuejs.org/config/
 module.exports = {
-  /**
-   * You will need to set publicPath if you plan to deploy your site under a sub path,
-   * for example GitHub Pages. If you plan to deploy your site to https://foo.github.io/bar/,
-   * then publicPath should be set to "/bar/".
-   * In most cases please use '/' !!!
-   * Detail: https://cli.vuejs.org/config/#publicpath
-   */
-  publicPath: '/',
+  publicPath: './',
   outputDir: 'dist',
   assetsDir: 'static',
   lintOnSave: process.env.NODE_ENV === 'development',
@@ -38,8 +26,6 @@ module.exports = {
     },
   },
   configureWebpack: {
-    // provide the app's title in webpack's name field, so that
-    // it can be accessed in index.html to inject the correct title.
     name: name,
     resolve: {
       alias: {
@@ -48,6 +34,15 @@ module.exports = {
     }
   },
   chainWebpack(config) {
+    /* config.optimization.minimizer([new TerserPlugin({
+      terserOptions: {
+        mangle: true, // 混淆，默认也是开的，mangle也是可以配置很多选项的，具体看后面的链接
+        compress: {
+          drop_console: true,//传true就是干掉所有的console.*这些函数的调用.
+          drop_debugger: true, //干掉那些debugger;
+        }
+      }
+    })]) */
     // it can improve the speed of the first screen, it is recommended to turn on preload
     config.plugin('preload').tap(() => [
       {
@@ -86,7 +81,7 @@ module.exports = {
             .plugin('ScriptExtHtmlWebpackPlugin')
             .after('html')
             .use('script-ext-html-webpack-plugin', [{
-            // `runtime` must same as runtimeChunk name. default is `runtime`
+              // `runtime` must same as runtimeChunk name. default is `runtime`
               inline: /runtime\..*\.js$/
             }])
             .end()
